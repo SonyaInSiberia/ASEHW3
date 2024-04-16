493a450ff7255ecb183fafa36bb6678d7515b028
tangy1227 <owentang1227@gmail.com>
2024-04-16 15:13:01 -0400

    update fd and td conv function

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -1,3 +1,8 @@
+extern crate rustfft;
+use rustfft::{FftPlanner, Fft};
+use rustfft::num_complex::Complex;
+use rustfft::num_traits::Zero;
+
 struct FastConvolver {
     // TODO: your fields here
 }
@@ -23,6 +28,44 @@ impl FastConvolver {
 
     pub fn flush(&mut self, output: &mut [f32]) {
         todo!("implement")
+    }
+
+    fn tdConv(signal: &[f32], kernel: &[f32]) -> Vec<f32> {
+        let signal_len = signal.len();
+        let kernel_len = kernel.len();
+        let output_len = signal_len + kernel_len - 1;
+        let mut output = vec![0.0; output_len];
+    
+        for n in 0..output_len {
+            let mut sum = 0.0;
+            for k in 0..kernel_len {
+                if n >= k && n - k < signal_len {
+                    sum += signal[n - k] * kernel[k];
+                }
+            }
+            output[n] = sum;
+        }
+    
+        output
+    }
+
+    fn fdConv(signal: &mut [Complex<f32>], kernel: &[Complex<f32>]){
+        let mut planner = FftPlanner::new();
+        let fft = planner.plan_fft(signal.len(), rustfft::FftDirection::Forward);
+    
+        // Perform FFT on both signals
+        fft.process(signal);
+        fft.process(&mut kernel.to_vec());
+    
+        // Multiply the frequency representations element-wise
+        for (a, b) in signal.iter_mut().zip(kernel.iter()) {
+            *a *= *b;
+        }
+    
+        // Perform inverse FFT
+        let mut planner = FftPlanner::new();
+        let ifft = planner.plan_fft(signal.len(), rustfft::FftDirection::Inverse);
+        ifft.process(signal);
     }
 
     // TODO: feel free to define other functions for your own use
